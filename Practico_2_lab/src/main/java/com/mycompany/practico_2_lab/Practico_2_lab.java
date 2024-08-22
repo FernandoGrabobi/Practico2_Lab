a5886a1f586b121044e216cf5dd8ddd0b35735b9
2657491274aZ <martinpere0987@gmail.com>
2024-08-22 19:53:40 -0300

    Update Practico_2_lab.java
    hola

--- Practico_2_lab/src/main/java/com/mycompany/practico_2_lab/Practico_2_lab.java
+++ Practico_2_lab/src/main/java/com/mycompany/practico_2_lab/Practico_2_lab.java
@@ -12,5 +12,9 @@ public class Practico_2_lab {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("hola");
     }
+    
+
 }
