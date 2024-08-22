318f1a1f6607187d97973b77ea6173bb4da1e648
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2024-08-22 19:08:26 -0300

    Proyecto tp2
    Aca se realizaran los cambios necesarios para el proyecto

---
+++ Practico_2_lab/src/main/java/com/mycompany/practico_2_lab/Practico_2_lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practico_2_lab;
+
+/**
+ *
+ * @author Lucia
+ */
+public class Practico_2_lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
